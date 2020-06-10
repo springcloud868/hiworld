f85c6ad9069ac297649c5071ed430ca4af433985
springcloud868 <melody_spring@163.com>
2020-06-10 13:54:25 +0800

    test2222222

--- src/main/java/com/cn/App.java
+++ src/main/java/com/cn/App.java
@@ -4,10 +4,9 @@ package com.cn;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("测试");
         System.out.println( "Hello World!" );
     }
 }
